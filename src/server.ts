d80a97e068a717b4e1c3c3d5ff3eca61d69b452e
CH_Hwang <hch950627@naver.com>
2022-09-17 17:50:18 +0900

    chore: setting
    db sync 문제: db sync할때 create if not exists가 되어야되는데 그냥 create를 한다.

---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+import * as Koa from 'koa';
+import * as koaBody from 'koa-body';
+import * as gracefulShutdown from 'http-graceful-shutdown';
+import { globalRouter } from './routes/api';
+import { db } from './libs/typeorm';
+// import apolloServer from './apollo-server'
+
+(async () => {
+  /**
+   * DB connect
+   */
+  db.initialize().then(() => console.log('DB Connected'));
+  const app = new Koa();
+  // const graphqlServer = await apolloServer()
+
+  app.use(koaBody({ multipart: true, jsonLimit: 10 * 1024 * 1024 })); // 10MB
+
+  /**
+   * Route middleware
+   */
+  app.use(globalRouter.middleware());
+  // await graphqlServer.start()
+  // app.use(graphqlServer.getMiddleware()) // path: /graphql
+
+  const server = app.listen(3000);
+
+  gracefulShutdown(server, {
+    signals: 'SIGINT SIGTERM',
+    timeout: 30000,
+    onShutdown: async () => {
+      console.log('The server shuts down when the connection is cleaned up.');
+      await db.destroy();
+    },
+    finally: () => {
+      console.log('bye 👋');
+      process.exit();
+    },
+  });
+
+  console.log('Server running on port 3000');
+})();
