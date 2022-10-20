202e7fb5b1a9144c2732c9fa1aa082b7b12c1c12
changhwan hwang <ch.hwang@ecubelabs.com>
2022-10-20 21:40:54 +0900

    feat: add transactional()

--- src/lib/ddd/ddd-service.ts
+++ src/lib/ddd/ddd-service.ts
@@ -1,7 +1,29 @@
 import { Inject } from 'typedi';
 import { DddContext } from './ddd-context';
+import { db } from '../typeorm';
 
 export class DddService {
   @Inject()
   context!: DddContext;
+}
+
+export function Transactional() {
+  return function (target: DddService, propertyKey: string, descriptor: PropertyDescriptor) {
+      const originalMethod = descriptor.value;
+      descriptor.value = async function (this: DddService) {
+          const thiz = this;
+          const args = arguments;
+          const originalEntityManager = this.context.entityManager;
+          let result: any;
+          await db.manager.transaction(async (entityManager) => {
+              // Replace entityManager set in DddContext
+              // We should use entityManger. see https://github.com/typeorm/typeorm/issues/2927
+              thiz.context.entityManager = entityManager;
+              result = await originalMethod.apply(thiz, args);
+          });
+          thiz.context.entityManager = originalEntityManager;
+          return result;
+      };
+      return descriptor;
+  };
 }
